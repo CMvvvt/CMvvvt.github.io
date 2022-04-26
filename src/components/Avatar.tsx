dcf8dc2196fadec973e71846ec2511d33335bf34
Ming Chen <452000761@qq.com>
2022-04-25 23:24:34 -0700

    feat: added intro page

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { classicNameResolver } from 'typescript';
+import '../styles/avatar.scss';
+
+type propsType = {
+  imageUrl: string;
+  name: string;
+};
+
+function Avatar({ imageUrl, name }: propsType) {
+  return (
+    <React.Fragment>
+      <div className="avatar">
+        {imageUrl && <img src={imageUrl} alt={name} />}
+        {imageUrl === '' && name !== '' && <div>{name}</div>}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Avatar;
---
+++ src/dict/index.ts
@@ -0,0 +1,39 @@
+export type item = {
+  id: number;
+  title: string;
+  name: string;
+  link: string;
+};
+
+export const items: item[] = [
+  {
+    id: 0,
+    title: 'facebook',
+    name: 'fab fa-facebook',
+    link: 'https://www.facebook.com/people/Ming-Chen/100034166210476',
+  },
+  {
+    id: 1,
+    title: 'instagram',
+    name: 'fab fa-instagram',
+    link: 'https://www.instagram.com/cmchenming13/?hl=en',
+  },
+  {
+    id: 2,
+    title: 'linkedin',
+    name: 'fab fa-linkedin',
+    link: 'https://www.linkedin.com/in/ming-chen-aa7387182/',
+  },
+  {
+    id: 3,
+    title: 'wechat',
+    name: 'fab fa-weixin',
+    link: 'https://www.linkedin.com/in/ming-chen-aa7387182/',
+  },
+  {
+    id: 4,
+    title: 'github',
+    name: 'fab fa-github',
+    link: 'https://github.com/CMvvvt/',
+  },
+];
